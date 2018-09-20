3160af62f7925df934b58afa2a3628c017c4c512
Gokul Swaminathan <gokulswamilive@gmail.com>
2018-09-19 19:45:03 -0700

    Added a song list

--- app/src/main/java/com/teamsoftware/materialmusic/MusicFragment.java
+++ app/src/main/java/com/teamsoftware/materialmusic/MusicFragment.java
@@ -1,35 +1,65 @@
 package com.teamsoftware.materialmusic;
 
-import android.content.ComponentName;
-import android.content.ContentResolver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.ServiceConnection;
-import android.database.Cursor;
-import android.net.Uri;
+import android.app.ProgressDialog;
+
 import android.os.Bundle;
-import android.os.IBinder;
+import android.os.Environment;
 import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.MediaController;
 
+import java.io.File;
+import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class MusicFragment extends Fragment{
+public class MusicFragment extends Fragment implements Serializable {
+
+    ListView listView;
+    String baseDirectory;
+    String[] listItems;
+    ArrayList<File> songList;
+    SongManager songManager;
+    ProgressDialog progressDialog;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootview = inflater.inflate(R.layout.fragment_music,container,false);
+        View rootview = inflater.inflate(R.layout.fragment_music, container, false);
 
+        init(rootview);
+
+        //songList updated again for assurance
+        songList = songManager.getSongsList();
+        listItems = new String[songList.size()];
+
+        //Remove the file extension '.mp3' from the name
+        for (int i = 0; i < songList.size(); i++) {
+            listItems[i] = songList.get(i).getName().replace(".mp3", "");
+        }
+
+        //ArrayAdapter for setting up the ListView
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getContext(), R.layout.row_layout, R.id.text_view, listItems);
+
+        if (listView != null) {
+            listView.setAdapter(arrayAdapter);
+
+            //Start MediaPlayer
+            //Send song position and songList
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    //Intent intent = new Intent(getContext(), /*Player class*/);
+                    //intent.putExtra("position", position);
+                    //startActivity(intent);
+                }
+            });
+        }
 
         return rootview;
     }
@@ -49,5 +79,26 @@ public class MusicFragment extends Fragment{
     @Override
     public void onPause() {
         super.onPause();
+    }
+
+    public void init(View v) {
+
+        listView = (ListView) v.findViewById(R.id.list_view);
+        songManager = new SongManager();
+        //Path to external storage directory, in this case SD card
+        baseDirectory = Environment.getExternalStorageDirectory().getAbsolutePath();
+
+        progressDialog = new ProgressDialog(getContext());
+        progressDialog.setMessage("Loading songs...");
+        //The dialog is cancelable with BACK key
+        progressDialog.setCancelable(true);
+
+        //Display the progress dialog, until all songs have been fetched
+        while (!songManager.getFetchStatus()) {
+            songList = songManager.findSongList(new File(baseDirectory));
+        }
+        if (songList != null) {
+            progressDialog.dismiss();
+        }
     }
 }
---
+++ app/src/main/java/com/teamsoftware/materialmusic/SongManager.java
@@ -0,0 +1,48 @@
+package com.teamsoftware.materialmusic;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class SongManager {
+
+    //Boolean value which determines whether all the songs have been fetched, or the process is ongoing
+    boolean fetchStatus = false;
+
+    //ArrayList for holding all the songs
+    ArrayList<File> songsList = new ArrayList<>();
+
+    /**
+     * Fetches all the songs in the sd card
+     * Inner folders are recursively checked for mp3 files
+     * Hidden folders are ignored as it may result in a stack-overflow
+     */
+    public ArrayList<File> findSongList(File root) {
+        ArrayList<File> songs = new ArrayList<>();
+        File[] files = root.listFiles();
+
+        for (File singleFile : files) {
+
+            if (singleFile.isDirectory() && !singleFile.isHidden()) {
+                songs.addAll(findSongList(singleFile));
+            } else {
+                if (singleFile.getName().endsWith(".mp3")) {
+                    songs.add(singleFile);
+                }
+            }
+
+            //All songs have been fetched
+            fetchStatus = true;
+            songsList = songs;
+
+        }
+        return songs;
+    }
+
+    public boolean getFetchStatus() {
+        return fetchStatus;
+    }
+
+    public ArrayList<File> getSongsList() {
+        return songsList;
+    }
+}
