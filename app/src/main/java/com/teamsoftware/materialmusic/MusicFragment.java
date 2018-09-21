90892ea02a603255fa87c82f68084ee240d9e4dc
nealpointerexception <neal20code@gmail.com>
2018-09-21 07:42:36 -0700

    add cache method

--- app/src/main/java/com/teamsoftware/materialmusic/MusicFragment.java
+++ app/src/main/java/com/teamsoftware/materialmusic/MusicFragment.java
@@ -9,12 +9,15 @@ import android.media.MediaMetadataRetriever;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.util.LruCache;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import com.mpatric.mp3agic.Mp3File;
 
 import java.io.File;
 import java.io.Serializable;
@@ -28,6 +31,9 @@ public class MusicFragment extends Fragment implements Serializable {
     ListView listView;
     String baseDirectory;
     ArrayList<File> songList;
+    ArrayList<Bitmap> albumArts;
+    ArrayList<Mp3File> songPreload;
+    LruCache<String, Bitmap> albumCache;
     SongManager songManager;
     ProgressDialog progressDialog;
     private static Context context;
@@ -38,13 +44,23 @@ public class MusicFragment extends Fragment implements Serializable {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootview = inflater.inflate(R.layout.fragment_music, container, false);
 
-        context = getActivity();
+        context = getContext();
         init(rootview);
 
         //songList updated again
         songList = songManager.getSongsList();
-
-        songadapter = new SongAdapter(context,songList);
+        albumArts = new ArrayList<>();
+        songPreload = new ArrayList<>();
+        for(File file : songList){
+            try {
+                songPreload.add(new Mp3File(file.getAbsolutePath()));
+                albumArts.add(getAlbumArt(songPreload.get(songPreload.size()-1)));
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+        songadapter = new SongAdapter(context,songList, albumArts, songPreload);
 
         if (listView != null) {
             listView.setAdapter(songadapter);
@@ -62,6 +78,15 @@ public class MusicFragment extends Fragment implements Serializable {
         }
 
         return rootview;
+    }
+    public Bitmap getAlbumArt(Mp3File file){
+
+        byte[] imageData = file.getId3v2Tag().getAlbumImage();
+        if (imageData != null) {
+            return BitmapFactory.decodeByteArray(imageData, 0, imageData.length);
+        }
+        return BitmapFactory.decodeResource(getContext().getResources(), R.drawable.ic_audiotrack_black_24dp);
+
     }
 
     @Override
--- app/src/main/java/com/teamsoftware/materialmusic/SongAdapter.java
+++ app/src/main/java/com/teamsoftware/materialmusic/SongAdapter.java
@@ -1,10 +1,12 @@
 package com.teamsoftware.materialmusic;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.media.MediaMetadataRetriever;
-import android.net.Uri;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,6 +18,7 @@ import android.widget.TextView;
 import com.mpatric.mp3agic.Mp3File;
 
 import java.io.File;
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -28,13 +31,15 @@ public class SongAdapter extends ArrayAdapter<File> {
 
     private Context mContext;
     private List<File> songsList;
-    private SongManager songManager;
+    private ArrayList<Bitmap> albumArts;
+    private ArrayList<Mp3File> preloadSongs;
     private Mp3File song;
-    public SongAdapter(@NonNull Context context, @LayoutRes ArrayList<File> list) {
+    public SongAdapter(@NonNull Context context, @LayoutRes ArrayList<File> list, @NonNull ArrayList<Bitmap> arts, @NonNull ArrayList<Mp3File> pre) {
         super(context, 0 , list);
         mContext = context;
         songsList = list;
-        songManager = new SongManager();
+        albumArts = arts;
+        preloadSongs = pre;
     }
 
     @NonNull
@@ -45,18 +50,19 @@ public class SongAdapter extends ArrayAdapter<File> {
             listItem = LayoutInflater.from(mContext).inflate(R.layout.row_layout,parent,false);
         Log.e("Song Path", songsList.get(position).getAbsolutePath());
 
-        try {
-            song = new Mp3File(songsList.get(position).getAbsolutePath());
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
+
 
         ImageView image = listItem.findViewById(R.id.image_view);
-        image.setImageBitmap(getAlbumArt(song));
-
+        image.setImageBitmap(albumArts.get(position));
+//        loadAlbum(position, image, song);
+        //new AlbumManager(image).execute(song);
+//        new Thread(new Runnable() {
+//            public void run() {
+//                image.setImageBitmap(getAlbumArt(song));
+//            }
+//        }).start();
         TextView name = listItem.findViewById(R.id.text_view);
-        name.setText(getMetadataAll(song).get("Title"));
+        name.setText(getMetadataAll(preloadSongs.get(position)).get("Title"));
 
         return listItem;
     }
@@ -78,13 +84,99 @@ public class SongAdapter extends ArrayAdapter<File> {
         return data;
 
     }
-    public Bitmap getAlbumArt(Mp3File file){
+//    public Bitmap getAlbumArt(Mp3File file){
+//
+//        byte[] imageData = file.getId3v2Tag().getAlbumImage();
+//        if (imageData != null) {
+//            return BitmapFactory.decodeByteArray(imageData, 0, imageData.length);
+//        }
+//        return BitmapFactory.decodeResource(mContext.getResources(), R.drawable.ic_audiotrack_black_24dp);
+//
+//    }
+//    private class AlbumManager extends AsyncTask<Mp3File, Integer, Bitmap> {
+//        private final WeakReference<ImageView> imageViewReference;
+//        private int data = 0;
+//
+//        public AlbumManager(ImageView imageView){
+//            imageViewReference = new WeakReference<ImageView>(imageView);
+//        }
+//
+//        @Override
+//        protected Bitmap doInBackground(Mp3File... mp3Files) {
+//
+//            byte[] imageData = mp3Files[data].getId3v2Tag().getAlbumImage();
+//            if (imageData != null) {
+//                return BitmapFactory.decodeByteArray(imageData, 0, imageData.length);
+//            }
+//            return null;
+//
+//
+//        }
+//
+//        @Override
+//        protected void onPostExecute(Bitmap bitmap) {
+//            if (isCancelled()) {
+//                bitmap = null;
+//            }
+//            if (bitmap != null) {
+//                final ImageView imageView = imageViewReference.get();
+//                final AlbumManager bitmapWorkerTask =
+//                        getAlbumManager(imageView);
+//                if (this == bitmapWorkerTask  && imageView != null) {
+//                    imageView.setImageBitmap(bitmap);
+//                }
+//            }
+//        }
+//    }
+//    static class AsyncDrawable extends BitmapDrawable {
+//        private final WeakReference<AlbumManager> albumManagerWeakReference;
+//
+//        public AsyncDrawable(Resources res, Bitmap bitmap,
+//                             AlbumManager bitmapWorkerTask) {
+//            super(res, bitmap);
+//            albumManagerWeakReference =
+//                    new WeakReference<AlbumManager>(bitmapWorkerTask);
+//        }
+//
+//        public AlbumManager getManager() {
+//            return albumManagerWeakReference.get();
+//        }
+//    }
+//    public void loadAlbum(int resId, ImageView imageView, Mp3File mp3File) {
+//        if (cancelPotentialWork(resId, imageView)) {
+//            final AlbumManager manager = new AlbumManager(imageView);
+//            final AsyncDrawable asyncDrawable =
+//                    new AsyncDrawable(mContext.getResources(), BitmapFactory.decodeResource(mContext.getResources(), R.drawable.ic_audiotrack_black_24dp), manager);
+//            imageView.setImageDrawable(asyncDrawable);
+//            manager.execute(mp3File);
+//        }
+//    }
+//    public static boolean cancelPotentialWork(int data, ImageView imageView) {
+//        final AlbumManager albumManager = getAlbumManager(imageView);
+//
+//        if (albumManager != null) {
+//            final int bitmapData = albumManager.data;
+//            if (bitmapData != data) {
+//                // Cancel previous task
+//                albumManager.cancel(true);
+//            } else {
+//                // The same work is already in progress
+//                return false;
+//            }
+//        }
+//        // No task associated with the ImageView, or an existing task was cancelled
+//        return true;
+//    }
+//    private static AlbumManager getAlbumManager(ImageView imageView) {
+//        if (imageView != null) {
+//            final Drawable drawable = imageView.getDrawable();
+//            if (drawable instanceof AsyncDrawable) {
+//                final AsyncDrawable asyncDrawable = (AsyncDrawable) drawable;
+//                return asyncDrawable.getManager();
+//            }
+//        }
+//        return null;
+//    }
 
-        byte[] imageData = file.getId3v2Tag().getAlbumImage();
-        if (imageData != null) {
-            return BitmapFactory.decodeByteArray(imageData, 0, imageData.length);
-        }
-        return null;
-    }
 
 }
