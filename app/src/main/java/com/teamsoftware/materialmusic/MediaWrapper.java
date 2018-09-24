d958abf1b70ae441a3abfd727e45ed58c60ec929
nealpointerexception <neal20code@gmail.com>
2018-09-23 21:38:48 -0700

    add music handler

---
+++ app/src/main/java/com/teamsoftware/materialmusic/MediaWrapper.java
@@ -0,0 +1,68 @@
+package com.teamsoftware.materialmusic;
+
+import android.media.MediaPlayer;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class MediaWrapper {
+    MetadataCacher cache;
+    private ArrayList<File> songList;
+    private File currSong;
+    private boolean looping;
+    private int currPos, maxSongs;
+    private MediaPlayer mediaPlayer;
+
+    public MediaWrapper(MetadataCacher cache, ArrayList<File> songList){
+        this.cache = cache;
+        this.songList = songList;
+        maxSongs = this.songList.size()-1;
+        mediaPlayer = new MediaPlayer();
+
+    }
+    public void setLoopingList(boolean x){
+        looping = x;
+    }
+    
+    public void playSong(int position){
+        currPos = position;
+        if(looping && position > maxSongs){
+            position = 0;
+        }
+        else{
+            position = -1;
+        }
+        if (position >= 0 && position <= maxSongs) {
+            currSong = songList.get(position);
+            try {
+                mediaPlayer.setDataSource(currSong.getAbsolutePath());
+                mediaPlayer.prepare();
+                mediaPlayer.start();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public void toggleCurrentSong(){
+        if(mediaPlayer.isPlaying()){
+            mediaPlayer.pause();
+        }
+        else{
+            mediaPlayer.start();
+        }
+
+    }
+    public void nextSong(){
+        playSong(currPos+1);
+    }
+    public void prevSong(){
+        playSong(currPos-1);
+
+
+    }
+
+    public MediaPlayer getMediaPlayer() {
+        return mediaPlayer;
+
+    }
+}
